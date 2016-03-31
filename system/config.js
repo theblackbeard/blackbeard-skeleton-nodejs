7692fa792159bb26800acade9c84024eae827641
BlackBeard <ccarvalhos@outlook.com>
2016-03-31 12:47:59 -0300

    BlackBeard Skeleton API

--- system/config.js
+++ system/config.js
@@ -3,10 +3,10 @@ module.exports = {
     'IPSERVER':  process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
     'PORTSERVER': process.env.OPENSHIFT_NODEJS_PORT || 8000,
     'MONGO': process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/',
-    'MONGODB': 'blackbeard',
+    'MONGODB': 'your db',
     'ROOT' : path.join(__dirname, '..'),
-    'CLOUDNAME' : 'djaysgwfa',
-    'APIKEY' : '998675877334278',
-    'APISECRETE': 'ObZXbU4RSFaEwTQ_8FYPNQd22IQ',
-    'SECRET': 'blackbeardecarvcomdemogimservercomodev'
+    'CLOUDNAME' : 'your clud name',
+    'APIKEY' : 'You api key',
+    'APISECRETE': 'Your api secrete',
+    'SECRET': 'any secrete you wanto to api token'
 }
